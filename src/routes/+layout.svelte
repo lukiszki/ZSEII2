ffe93d5cd2a71d2151d2a45c1eec473b3e80cf9d
lukiszki <zameleklukasz@gmail.com>
2024-10-13 20:37:39 +0200

    completed main page frontend

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,42 @@
 <script lang="ts">
     import "../app.css"
-    import { Terminal } from "lucide-svelte";
+    import { slide } from "svelte/transition";
+    import { Terminal,Menu } from "lucide-svelte";
+    let isMenuOpen = false;
 </script>
 
 
-<header class='bg-gray-800 p-4 px-16 sticky top-0 z-10 shadow-lg'>
-    <Terminal/><p class='text-2xl font-bold text-white'>ZSEII</p>
-
+<header class='bg-gray-800 p-4 px-8 sticky top-0 z-10 shadow-lg'>
+    <div class='max-w-[1024px] mx-auto px-4'>
+        <div class="flex gap-4 justify-between">
+            <div class='flex gap-4'>
+                <div class="flex items-center gap-2">
+                    <Terminal class='text-emerald-400' size={26}/><p class='text-2xl font-bold text-white'>ZSEII</p>
+                </div>
+            <div class='hidden md:flex items-center gap-2'>
+                    <div class='transition hover:bg-gray-900 p-2 rounded-lg cursor-pointer'>Poradniki</div>
+                    <div class='transition hover:bg-gray-900 p-2 rounded-lg cursor-pointer'>Narzędzia</div>
+                    <div class='transition hover:bg-gray-900 p-2 rounded-lg cursor-pointer'>Kontakt</div>
+                    <div class='transition hover:bg-gray-900 p-2 rounded-lg cursor-pointer'>Forum</div>
+                    <div class='transition hover:bg-gray-900 p-2 rounded-lg cursor-pointer'>Zaproponuj funkcje</div>
+                </div>
+            </div>
+            <button class="md:hidden text-white" on:click={() => isMenuOpen = !isMenuOpen}>
+            <Menu size={24} />
+            </button>
+        </div>
+        {#if isMenuOpen}
+            <div transition:slide={{ duration:300}} class='md:hidden py-4'>
+                <div  class='flex flex-col'>
+                    <div class='transition hover:bg-gray-900 p-2 rounded-lg cursor-pointer'>Poradniki</div>
+                    <div class='transition hover:bg-gray-900 p-2 rounded-lg cursor-pointer'>Narzędzia</div>                
+                    <div class='transition hover:bg-gray-900 p-2 rounded-lg cursor-pointer'>Forum</div>
+                    <div class='transition hover:bg-gray-900 p-2 rounded-lg cursor-pointer'>Kontakt</div>
+                    <div class='transition hover:bg-gray-900 p-2 rounded-lg cursor-pointer'>Zaproponuj funkcje</div>
+                </div>
+            </div>
+    {/if}
+ </div>
 </header>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,35 @@
 <script lang="ts"> 
-  import { BookText,Drill } from "lucide-svelte";
+  import { BookText,Drill,MessageSquareCode } from "lucide-svelte";
 </script>
 
-<header class='max-w-[768px] mx-auto my-6'>
-  <h1 class='text-4xl text-emerald-500 font-bold text-center my-2'>Witaj w ZSEII</h1>
+<header class='max-w-4xl mx-auto my-6 p-2'>
+  <h1 class='text-4xl text-emerald-400 font-bold text-center my-2'>Witaj w ZSEII</h1>
   <h3 class='text-lg text-white text-center my-2'>Twoje centrum wiedzy i innowacji w świecie IT. Odkryj zaawansowane poradniki, praktyczne narzędzia i społeczność pasjonatów technologii.</h3>
 </header>
-<main class='max-w-[900px] mx-auto my-20'>
-  <h2 class='text-xl text-emerald-500 font-semibold text-center my-6'>Odkryj nasze zasoby</h2>
-  <section class='w-full h-36 bg-gray-800 rounded-md shadow-xl my-6 p-6'>
-   <BookText class='mx-auto text-emerald-500' size={32}/>
+<main class='max-w-[1024px] mx-auto my-4` p-4'>
+  <h2 class='text-xl text-emerald-400 font-semibold text-center my-6'>Odkryj nasze zasoby</h2>
+  <section class='w-full min-h-36 bg-gray-800 rounded-md shadow-xl my-6 p-6'>
+   <BookText class='mx-auto text-emerald-400' size={32}/>
    <p class='text-center text-lg text-white font-bold m-2'>Poradniki</p>
    <p class='text-center text-slate-400'>Praktyczna wiedza z zakresu sieci, programowania i systemów</p>
   </section>
-  <section class='w-full h-36 bg-gray-800 rounded-md shadow-xl my-6 p-6'>
-   <Drill class='mx-auto text-emerald-500' size={32}/>
+  <section class='w-full min-h-36 bg-gray-800 rounded-md shadow-xl my-6 p-6'>
+   <Drill class='mx-auto text-emerald-400' size={32}/>
    <p class='text-center text-lg text-white font-bold m-2'>Narzędzia</p>
    <p class='text-center text-slate-400'>Zestaw przydatnych narzędzi dla każdego informatyka</p>
   </section>
+   <section class='w-full min-h-36 bg-gray-800 rounded-md shadow-xl my-6 p-6'>
+   <MessageSquareCode class='mx-auto text-emerald-400' size={32}/>
+   <p class='text-center text-lg text-white font-bold m-2'>Forum</p>
+   <p class='text-center text-slate-400'>Społeczność ekspertów i pasjonatów IT dzieląca się wiedzą i doświadczeniem</p>
+  </section>
    
 </main>
-<article class='max-w-[900px] mx-auto my-20'>
-  <h2 class='text-xl text-emerald-500 font-semibold text-left my-6'>Ostatnie poradniki</h2>
+<article class='max-w-[1024px] mx-auto my-4 p-4'>
+  <h2 class='text-xl text-emerald-400 font-semibold text-left my-6'>Ostatnie poradniki</h2>
   {#each [1,2,3,4,5] as l} 
-    <section class='w-full h-32 bg-gray-800 rounded-md shadow-xl my-6'>
-      <p class='text-4md text-emerald-500 font-bold p-4 pb-2'>Tytuł poradnika</p>
+    <section class='min-h-32 bg-gray-800 rounded-md shadow-xl my-6 p-1'>
+      <p class='text-4md text-emerald-400 font-bold p-4 pb-2'>Tytuł poradnika</p>
       <p class='text-md text-slate-400 m-4 mt-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis vero dolore corporis sequi modi! Enim ipsum aperiam consequuntur, iste error esse illum alias doloremque laudantium, id dicta reiciendis sapiente expedita?</p>
     </section>
   {/each}
